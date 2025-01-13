a5c1cc53ea39e3103bc145e9deba128137c97448
Руденко Вячеслав Александрович <99740603+Slava47@users.noreply.github.com>
2025-01-13 10:11:57 +0300

    Update VolonterBot.py

--- VolonterBot.py
+++ VolonterBot.py
@@ -23,7 +23,7 @@ TOKEN = os.getenv('TOKEN')  # Используйте имя переменной
 bot = telebot.TeleBot(TOKEN)
 
 # Подключение к базе данных
-conn = sqlite3.connect('/app/data/volunter_bot.db', check_same_thread=False)
+conn = sqlite3.connect('/app/data/volunteer_bot.db', check_same_thread=False)
 cursor = conn.cursor()
 
 cursor.execute('''
